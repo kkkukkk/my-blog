831c705e130b50dc9d79e491396404bf7121cc16
DESKTOP-DPDSLQD\JinUk <jin960828@naver.com>
2024-10-29 13:32:29 +0900

    test6

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     distDir: 'out',
     images: {
         unoptimized: true,
-    }
+    },
+    basePath: '/my-blog',       // 리포지토리 이름으로 설정
+    assetPrefix: '/my-blog',
 };
 
 export default nextConfig;
