99f8522a10bb6b9d5075e613509a8f7f045fb914
DESKTOP-DPDSLQD\JinUk <jin960828@naver.com>
2024-10-29 13:22:33 +0900

    test4

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    dsitDir: 'out',
+    distDir: 'out',
     images: {
         unoptimized: true,
     }
